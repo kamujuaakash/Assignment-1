c85c5ba1886ca94767594e35fb3ee73749f07c15
kamujuaakash <102615028+kamujuaakash@users.noreply.github.com>
2022-04-01 20:39:49 +0530

    Add files via upload

---
+++ h.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main(){
+float x,y;
+int a,b;
+//After solving the equations in matrices
+/*
+2*y = -4
+2*x = 6
+*/
+
+a = 2;
+b = 2;
+
+x = 6/a;
+y = -4/b;
+
+printf("x,y are %f,%f",x,y);
+return 0;
+}
