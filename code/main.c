c4ffebe59942d510bb15071206c846802c34bf13
kamujuaakash <102615028+kamujuaakash@users.noreply.github.com>
2022-04-02 18:35:23 +0530

    Update and rename p to main.c

---
+++ code/main.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+int main(){
+float x,y;
+int a,b;
+//After solving the equations in matrices
+/*
+2*y = -4
+2*x = 6
+*/
+
+a = 2;
+b = 2;
+
+x = 6/a;
+y = -4/b;
+
+printf("x,y are %f,%f",x,y);
+return 0;
+}
+© 2022 GitHub, Inc.
+Terms
+Privacy
+Security
+Status
+Docs
+Contact GitHub
+Pricing
+API
+Training
+Blog
+About
+
