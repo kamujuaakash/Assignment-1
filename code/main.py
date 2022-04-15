5b6254d4dcd89f5c12a4665caa9eb4293252f167
kamujuaakash <102615028+kamujuaakash@users.noreply.github.com>
2022-04-15 12:02:35 +0530

    Update main.py

--- code/main.py
+++ code/main.py
@@ -1,6 +1,6 @@
-from mpmath.libmp.libmpf import round_int
 from mpmath import *
-mp.dps = 25; 
+mp.dps = 0; 
 a = sin(pi*34/180)**2 + sin(pi*56/180)**2 + 2*tan(pi*18/180)*tan(pi*72/180)
-b =    cot(pi/6)**2
-print(round(a-b,1))
+b = cot(pi/6)**2
+d = b
+a - d
