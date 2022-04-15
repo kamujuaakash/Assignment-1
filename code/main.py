eaa1d426e72e8b0d7fd8a7450a51617f38990a64
kamujuaakash <102615028+kamujuaakash@users.noreply.github.com>
2022-04-15 11:50:28 +0530

    Update main.py

--- code/main.py
+++ code/main.py
@@ -1,12 +1,6 @@
-from mpmath import mp
-import math
-mp.dps = 1
-a = math.sin(math.pi*34/180)
-b = math.sin(math.pi*56/180)
-c = math.tan(math.pi*18/180)
-d = math.tan(math.pi*72/180)
-e = math.tan(math.pi*60/180)
-result1 = mpf(a*a + b*b + 2*c*d )
-result2 = mpf(e*e)
-result = result1 - result2
-print(result)
+from mpmath.libmp.libmpf import round_int
+from mpmath import *
+mp.dps = 25; 
+a = sin(pi*34/180)**2 + sin(pi*56/180)**2 + 2*tan(pi*18/180)*tan(pi*72/180)
+b =    cot(pi/6)**2
+print(round(a-b,1))
